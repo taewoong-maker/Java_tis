8c352d4b0ec7b036148d62605cfc2b02a0a09401
taewoong-maker <66583403+taewoong-maker@users.noreply.github.com>
2020-08-25 23:52:59 +0900

    알고리즘 수정

--- Algorithm/practice02/DailyTemperature.java
+++ Algorithm/practice02/DailyTemperature.java
@@ -8,12 +8,22 @@ public class DailyTemperature {
 
 	public static void main(String[] args) {
 		
-		int[] nums= {73,74,76,71,69,72,76,73};
+		int[] nums= {73,74,76,71,69,72,86,73};
+		int[] degs= new int[nums.length];
+		
 		for(int i=0;i<nums.length;i++) {
 			for(int j=i+1;j<nums.length;j++) {
-				
+				if(nums[i]<nums[j]) {
+					degs[i]=j-i;
+					break;	
+				}else if(j==degs.length){
+					degs[i]=0;
+				}					
 			}
 		}
+		
+		for(int i:degs)
+			System.out.println(i);
 	}
 	
 	
